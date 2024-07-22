da2a10c9a02eb153b5e363f5aa964f40599836b0
Son Le <sonlele2000@gmail.com>
2024-07-22 22:12:52 +0700

    fix FE domain CORS

--- src/app/bootstrap/bootstrap.ts
+++ src/app/bootstrap/bootstrap.ts
@@ -22,7 +22,7 @@ export class App {
     );
     app.use(helmet());
     app.enableCors({
-      origin: ['http://localhost:3000', 'http://localhost:3030', 'https://dev-blog-fe.vercel.app', 'https://dev-blog-admin.vercel.app'],
+      origin: ['http://localhost:3000', 'http://localhost:3030', 'https://devblogsite.vercel.app', 'https://dev-blog-admin.vercel.app'],
       methods: 'GET, HEAD, PUT, POST, DELETE, OPTIONS, PATCH',
       credentials: true,
       allowedHeaders: 'Content-Type, Authorization',
